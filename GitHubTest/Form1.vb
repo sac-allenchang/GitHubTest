9d5ea99b3d33496ca541d2ee6108c6795f579a39
allen <allen.chang@sunspringautomation.com.tw>
2017-06-02 10:20:20 +0800

    Add 2nd button to show msg.

--- GitHubTest/Form1.vb
+++ GitHubTest/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Hello World!")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Hello World2!")
+    End Sub
 End Class
